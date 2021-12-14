66a055c420fed7b10d17a18a24a873f5e2dd1d52
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-14 17:53:38 -0500

    built and render log in component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<ul>
+				<li>
+					<Link to='/login'>Login</Link>
+				</li>
+			</ul>
+			<Switch>
+				<Route path='/login' component={Login} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
